65a775a146a9de61e187415b8b4fee353dbfe2c3
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-05-27 22:05:46 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,30 @@
+def reverse_each_word(string)
+  new_string = ""
+  string_to_array = string.split(" ")
+  string_to_array.each do |word|
+    if word == string_to_array[string_to_array.length - 1]
+      new_string << " #{word.reverse}"
+    elsif word == string_to_array[0]  
+      new_string << "#{word.reverse}"
+    else  
+      new_string << " #{word.reverse}" 
+    end
+  end 
+  new_string
+end  
+
+def reverse_each_word(string)
+  string_to_array = string.split(" ")
+  reversed_arr = string_to_array.collect do |word|
+    word.reverse
+  end  
+  reversed_arr.join(" ")
+end  
+  
+  
+
+
+
+
+
+  
