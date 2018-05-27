b1b0eb1f33db3188c179e8b6108a24b697db570e
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-05-27 21:13:06 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,11 @@ def reverse_each_word(string)
   new_string = ""
   string_to_array = string.split(" ")
   string_to_array.each do |word|
-    new_string << word.reverse 
+    if word == string_to_array[string_to_array.length - 1]
+      new_string << " #{word.reverse}"
+    else  
+      new_string << " #{word.reverse} " 
+    end
   end 
   new_string
 end  
