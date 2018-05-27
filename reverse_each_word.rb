decf4cc7f5e289097369bc27ca9ccb883b00baf9
Sarah Rivas <sarah.r.rivas@gmail.com>
2018-05-27 20:49:51 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(string)
   new_string = ""
-  string.each do |word|
+  string_to_array = string.split(" ")
+  string_to_array.each do |word|
     new_string << word.reverse 
   end 
   new_string
